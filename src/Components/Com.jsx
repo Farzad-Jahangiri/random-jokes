4616a77b941d9624e1496cce19117d529c8c7c0f
Farzad92.j <Farzad92.j@gmail.com>
2023-07-13 17:09:30 +0330

    update axios API

--- src/Components/Com.jsx
+++ src/Components/Com.jsx
@@ -4,37 +4,37 @@ import Image from "./1.png";
 import axios from "axios";
 
 const GetNewJokes = async () => {
-  let joke ="";
+  let joke = "";
   const url =
     "https://api.humorapi.com/jokes/random?api-key=8030a698901d46a0946cc0281134f3fa&max-length=140";
   try {
     const response = await axios.get(url);
     const data = response.data;
     joke = data;
-  } catch(error) {
+  } catch (error) {
     joke = error.message;
   }
   return joke;
 };
 
 function Com() {
-  const [textElementJoke,setTextElementJoke] = useState("Click below to read a joke")
+  const [textElementJoke, setTextElementJoke] = useState(
+    "Click below to read a joke"
+  );
 
-  const BTNgetJokeHandler = ()=>{
-    setTextElementJoke("LOADING...")
-    GetNewJokes().then((response)=>{
-      setTextElementJoke(response.joke)
+  const BTNgetJokeHandler = async() => {
+    setTextElementJoke("LOADING...");
+    await GetNewJokes().then((response) => {
+      if (response.joke) setTextElementJoke(response.joke);
+      else setTextElementJoke(response);
     });
-    // console.log(myJoke);
-  }
+  };
 
   return (
     <div className="Com">
       <nav className="com1">
         <img src={Image} alt="" />
-        <p className="one">
-          {textElementJoke}
-        </p>
+        <p className="one">{textElementJoke}</p>
         <button onClick={BTNgetJokeHandler} type="button" className="btn">
           Get Random Joke
         </button>
